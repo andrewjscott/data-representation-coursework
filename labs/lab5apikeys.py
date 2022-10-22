01feaf393c9010ede06b88fc4d291e7c7624e262
Andrew <G00398249@gmit.ie>
2022-10-22 13:12:28 +0100

    starting lab 5. Adding gitignore

---
+++ labs/lab5apikeys.py
@@ -0,0 +1,23 @@
+#  Write a script that will convert the Wikipedia website into a pdf
+
+import requests
+import urllib.parse
+from config import config as cfg
+
+target_url = "https://en.wikipedia.org"
+
+api_key = cfg["htmltopdf_key"]
+
+api_url = "https://api.html2pdf.app/v1/generate"
+
+params = {"url": target_url, "apiKey": api_key}
+parsed_params = urllib.parse.urlencode(params)
+request_url = api_url + "?" + parsed_params
+
+response = requests.get(request_url)
+print(response.status_code)
+
+result = response.content
+
+with open("document.pdf", "wb") as handler:
+    handler.write(result)
